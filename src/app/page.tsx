4d3e00467248fbe77752d3c577695da56ea12f8c
Ravish Bisht <ravishbisht85@gmail.com>
2024-12-09 10:30:31 +0530

    test github deploy automation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -95,7 +95,7 @@ export default function Page() {
                   disabled={loading}
                 >
                   {!loading ? (
-                    "Start Chat"
+                    "Start Chats"
                   ) : (
                     <div className="flex justify-center items-center gap-2">
                     connecting...
